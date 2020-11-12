273859b818bcc0dab7741390d9ee750c877a71c0
Jaap Koetsier <jaap@chariot-it.nl>
2020-11-12 14:11:08 +0100

    Add new Decos configuration. Add 'default' flag to imports to avoid having to change all jobs.

--- gobconfig/import_/data/sql/bag/_deprecated.brondocumenten.sql
+++ gobconfig/import_/data/sql/bag/_deprecated.brondocumenten.sql
@@ -1,9 +1,8 @@
 SELECT *
-FROM
-(
-   SELECT b.subject1, b.subject2                                 AS documentnummer
-   ,      UPPER(SUBSTRING(b.subject2, 1, 2))                     AS bronleverancier_code
-   ,      CASE UPPER(SUBSTRING (b.subject2, 1, 2))
+FROM   (
+   SELECT b.subject2                                          AS documentnummer
+   ,      UPPER(substr(b.subject2, 1, 2))                     AS bronleverancier_code
+   ,      CASE UPPER(substr(b.subject2, 1, 2))
            WHEN 'SA' THEN 'Stadsdeel Centrum'
            WHEN 'SC' THEN 'Stadsdeel Westerpark'
            WHEN 'SD' THEN 'Stadsdeel Oud-West'
@@ -33,9 +32,9 @@ FROM
            WHEN 'TM' THEN 'Terugmelding'
            WHEN 'SB' THEN 'Straatnaambesluit'
            ELSE NULL
-          END                                                    AS bronleverancier_omschrijving
-   ,      UPPER(SUBSTRING(b.subject2, 12, 2))                    AS typedossier_code
-   ,      CASE UPPER(SUBSTRING(b.subject2, 12, 2))
+          END                                                AS bronleverancier_omschrijving
+   ,      UPPER(substr(b.subject2, 12, 2))                   AS typedossier_code
+   ,      CASE UPPER(substr(b.subject2, 12, 2))
            WHEN 'OV' THEN 'Omgevingsvergunning'
            WHEN 'BL' THEN 'Lichte bouwvergunning'
            WHEN 'NB' THEN 'Losse nummerbeschikking'
@@ -49,9 +48,9 @@ FROM
            WHEN 'WO' THEN 'Woningonttrekking zonder omgevingsvergunning'
            WHEN 'TM' THEN 'Terugmelding'
            ELSE NULL
-          END                                                   AS typedossier_omschrijving
-   ,      UPPER(SUBSTRING(b.subject2, 16, 2))                   AS typebrondocument_code
-   ,      CASE UPPER(SUBSTRING(b.subject2, 16, 2))
+          END                                                AS typedossier_omschrijving
+   ,      UPPER(substr(b.subject2, 16, 2))                   AS typebrondocument_code
+   ,      CASE UPPER(substr(b.subject2, 16, 2))
            WHEN 'VL' THEN 'Voorloopvel'
            WHEN 'BA' THEN 'Aanvraag omgevingsvergunning voor bouwen/slopen/splitsing/woningonttrekking'
            WHEN 'VG' THEN 'Omgevingsvergunning voor bouwen/slopen/splitsing/woningonttrekking'
@@ -72,13 +71,13 @@ FROM
            WHEN 'TM' THEN 'Terugmelding (in onderzoek zetten)'
            WHEN 'OP' THEN 'Terugmelding (uit onderzoek halen)'
            ELSE NULL
-          END                                                   AS typebrondocument_omschrijving
-   ,      b.item_created                                        AS registratiedatum
-   ,      b.item_key                                            AS source_id
-   FROM   item b
-   JOIN   item d ON d.item_key = b.it_parent_key
+          END                                                AS typebrondocument_omschrijving
+   ,      b.item_created                                     AS registratiedatum
+   ,      b.item_key                                         AS source_id
+   FROM   decos.item b
+   JOIN   decos.item d ON d.item_key = b.it_parent_key
    WHERE  d.item_deleted IS NULL
    AND    b.item_deleted IS NULL
    AND    b.subject2 IS NOT NULL
-   AND    UPPER(SUBSTRING(b.subject1, 8, 2)) IN ('GV', 'MB', 'PC', 'RO', 'SA', 'SB', 'SC', 'SD', 'SE', 'SF', 'SG', 'SH', 'SJ', 'SK', 'SM', 'SN', 'SP', 'SQ', 'SR', 'ST', 'SU', 'SV', 'SW', 'TM')
-) brondocumenten
+   AND    UPPER(substr(b.subject1, 8, 2)) IN ('GV', 'MB', 'PC', 'RO', 'SA', 'SB', 'SC', 'SD', 'SE', 'SF', 'SG', 'SH', 'SJ', 'SK', 'SM', 'SN', 'SP', 'SQ', 'SR', 'ST', 'SU', 'SV', 'SW', 'TM')
+   )
--- gobconfig/import_/data/sql/bag/dossiers.sql
+++ gobconfig/import_/data/sql/bag/dossiers.sql
@@ -1,13 +1,22 @@
 SELECT *
-FROM   (
-   SELECT regexp_substr(b.subject2, '^(.*?)[_ ]', 1, 1, NULL, 1)         AS dossier
-   ,      MAX(b.item_created) AS registratiedatum
-   ,      rtrim( xmlcast( xmlagg( xmlelement(e, b.subject2 || ';') order by b.subject2) as clob), ';') AS heeft_bag_brondocument
-   FROM   decos.item b
-   JOIN   decos.item d ON d.item_key = b.it_parent_key
-   WHERE  d.item_deleted IS NULL
-   AND    b.item_deleted IS NULL
-   AND    b.subject2 IS NOT NULL
-   AND    UPPER(substr(b.subject1, 8, 2)) IN ('GV', 'MB', 'PC', 'RO', 'SA', 'SB', 'SC', 'SD', 'SE', 'SF', 'SG', 'SH', 'SJ', 'SK', 'SM', 'SN', 'SP', 'SQ', 'SR', 'ST', 'SU', 'SV', 'SW', 'TM')
-   GROUP BY regexp_substr(b.subject2, '^(.*?)[_ ]', 1, 1, NULL, 1)
-   )
+FROM (
+         SELECT dossier                                             AS dossier,
+                max(item_created)                                   AS registratiedatum,
+                string_agg(cast(subject2 as nvarchar(max)), ';')
+                within group (order by subject2)                    AS heeft_bag_brondocument
+         FROM (
+                  SELECT substring(b.subject2, 0, patindex('%[_ ]%', b.subject2)) AS dossier
+                       , b.item_created
+                       , b.subject2
+                  FROM dbo.item b
+                           JOIN dbo.item d ON d.item_key = b.it_parent_key
+                  WHERE d.item_deleted IS NULL
+                    AND b.item_deleted IS NULL
+                    AND b.subject2 IS NOT NULL
+                    AND UPPER(substring(b.subject1, 8, 2)) IN
+                        ('GV', 'MB', 'PC', 'RO', 'SA', 'SB', 'SC', 'SD', 'SE', 'SF', 'SG', 'SH', 'SJ', 'SK', 'SM',
+                         'SN',
+                         'SP', 'SQ', 'SR', 'ST', 'SU', 'SV', 'SW', 'TM')
+              ) q
+         GROUP BY dossier
+     ) r
