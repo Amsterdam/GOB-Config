f2cf277a7c86ec57212a6cebe97614aa477435c7
Jaap Koetsier <jaap@chariot-it.nl>
2020-12-08 13:19:11 +0100

    Explicitly add eind_geldigheid to woz imports

--- gobconfig/import_/data/sql/woz/wozobjecten.sql
+++ gobconfig/import_/data/sql/woz/wozobjecten.sql
@@ -5,5 +5,6 @@ select wozobjectnummer,
        soortobject_code,
        soortobject_oms,
        begin_geldigheid,
+       NULL as eind_geldigheid,
        bevat_kadastraal_object
 from woz_prepared.object
