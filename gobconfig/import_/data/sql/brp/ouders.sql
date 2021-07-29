47fdc0d55148ae26a9d91cd570c132dc4fce41b3
Michiel <m.kuik@amsterdam.nl>
2021-07-29 14:26:52 +0200

    Adding Relations to personen

---
+++ gobconfig/import_/data/sql/brp/ouders.sql
@@ -0,0 +1,13 @@
+select identificatie
+,      burgerservicenummer
+,      voornamen
+,      voorvoegsels
+,      geslachtsnaam
+,      geslachtsaanduiding_code
+,      geslachtsaanduiding_oms
+,      geboortedatum
+,      geboorteplaats_oms
+,      geboorteplaats_pl_in_buitlnd
+,      geboorteplaats_heeft_gem_code
+,      geboorteland
+from brp_prepared.ouders
---
+++ gobconfig/import_/data/sql/brp/overlijden.sql
@@ -0,0 +1,5 @@
+select identificatie
+,      datum_overlijden
+,      plaats_overlijden
+,      land_overlijden
+from brp_prepared.overlijden
