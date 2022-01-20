7c2f00f08bd5a9dbd9f8c25f4ea9acd34d3e6779
Roel de Vries <roel.de.vries@amsterdam.nl>
2022-01-20 17:52:27 +0100

    Import correct codes after changes 24-03-2022
    #26070

--- gobconfig/import_/data/sql/gebieden/buurten.sql
+++ gobconfig/import_/data/sql/gebieden/buurten.sql
@@ -1,12 +1,23 @@
+-- Due to changes in the coding system for wijken, add a case for before and after the change on 24-03-2022
+-- Stadsdeel is already contained in the wijk code
+-- before: code = stadsdeel + wijk + buurt, after: code = wijk + buurt
 SELECT s1.naam                                                             AS naam
-,      s1.sdlcode || s1.wijkcode || s1.brtcode                             AS code
+,      CASE
+       WHEN nvl(q2.einddatum, q3.ingsdatum) <= to_date('2022-03-24', 'YYYY-MM-DD')
+           THEN s1.sdlcode || s1.wijkcode || s1.brtcode
+       ELSE s1.wijkcode || s1.brtcode
+       END                                                                 AS code
 ,      NULL                                                                AS identificatie
 ,      q2.volgnummer                                                       AS volgnummer
 ,      to_char(q2.ingsdatum, 'YYYY-MM-DD')                                 AS begin_geldigheid
 ,      to_char(nvl(q2.einddatum, q3.ingsdatum), 'YYYY-MM-DD')              AS eind_geldigheid
 ,      to_char(s1.docdatum, 'YYYY-MM-DD')                                  AS documentdatum
 ,      s1.docnr                                                            AS documentnummer
-,      s1.sdlcode || s1.wijkcode                                           AS ligt_in_wijk
+,      CASE
+       WHEN nvl(q2.einddatum, q3.ingsdatum) <= to_date('2022-03-24', 'YYYY-MM-DD')
+           THEN s1.sdlcode || s1.wijkcode
+       ELSE s1.wijkcode
+       END                                                                 AS ligt_in_wijk
 ,      to_char(t1.inwin, 'YYYY-MM-DD HH24:MI:SS')                          AS registratiedatum
 ,      t1.guid                                                             AS source_id
 ,      to_char(nvl(q2.einddatum, q3.ingsdatum), 'YYYY-MM-DD HH24:MI:SS')   AS expirationdate
@@ -27,4 +38,4 @@ JOIN  (SELECT t2.id
                          FROM   gebieden.dgdtw_topografie t3
                          JOIN   gebieden.dgdtw_table_6021 s3 ON t3.id = s3.dgdtw_primary_key) q3 ON t1.guid = q3.guid AND t1.verval = q3.inwin
 WHERE  t1.objectcode = 6021 -- buurt
-ORDER  BY s1.sdlcode, s1.wijkcode || s1.brtcode, q2.volgnummer
+ORDER BY code, volgnummer
