346aea6920d2a9956b166085d097b562c1cf6468
Roel de Vries <roel.de.vries@amsterdam.nl>
2022-04-28 16:02:48 +0200

    Derive volgnummer lastly, so we start with 1

--- gobconfig/import_/data/sql/gebieden/stadsdelen.sql
+++ gobconfig/import_/data/sql/gebieden/stadsdelen.sql
@@ -1,6 +1,6 @@
 SELECT s1.naam                                                             AS naam
 ,      s1.sdlcode                                                          AS code
-,      q2.volgnummer                                                       AS volgnummer
+,      dense_rank() OVER (partition BY q2.guid ORDER BY q3.inwin)          AS volgnummer
 ,      to_char(q2.ingsdatum, 'YYYY-MM-DD')                                 AS begin_geldigheid
 ,      to_char(nvl(q2.einddatum, q3.ingsdatum), 'YYYY-MM-DD')              AS eind_geldigheid
 ,      to_char(s1.docdatum, 'YYYY-MM-DD')                                  AS documentdatum
@@ -15,7 +15,7 @@ JOIN   gebieden.dgdtw_table_6027 s1 ON t1.id = s1.dgdtw_primary_key
 JOIN  (SELECT t2.id
        ,      s2.ingsdatum
        ,      s2.einddatum
-       ,      dense_rank() OVER (partition BY t2.guid ORDER BY t2.inwin) AS volgnummer
+       ,      t2.guid
        FROM   gebieden.dgdtw_topografie t2
        JOIN   gebieden.dgdtw_table_6027 s2 ON t2.id = s2.dgdtw_primary_key) q2 ON t1.id = q2.id
        -- einddatum toestand selecteren (kan leeg zijn) van volgende cyclus
@@ -26,4 +26,4 @@ JOIN  (SELECT t2.id
                          JOIN   gebieden.dgdtw_table_6027 s3 ON t3.id = s3.dgdtw_primary_key) q3 ON t1.guid = q3.guid AND t1.verval = q3.inwin
 WHERE  t1.objectcode = 6027 -- stadsdeel
         AND (q2.ingsdatum <> nvl(q2.einddatum, q3.ingsdatum) OR nvl(q2.einddatum, q3.ingsdatum) IS NULL) -- exclude intervals with length 0
-ORDER BY s1.sdlcode, q2.volgnummer
+ORDER BY s1.sdlcode, volgnummer
