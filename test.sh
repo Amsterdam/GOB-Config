af6d6e9bafeed1e2e58cccedaa658772cea29534
Peter Adrichem <Peter.Adrichem@gmail.com>
2022-09-01 13:55:09 +0200

    Updated test requirements.

--- test.sh
+++ test.sh
@@ -3,14 +3,11 @@
 set -u # crash on missing env
 set -e # stop on any error
 
-# Coverage 6: coverage run --data-file=/tmp/.coveragerc …
-export COVERAGE_FILE=/tmp/.coverage
-
 echo "Running style checks"
 flake8
 
 echo "Running unit tests"
-coverage run --source=./gobconfig -m pytest tests/
+coverage run --data-file=/tmp/.coveragerc --source=./gobconfig -m pytest tests/
 
 echo "Coverage report"
-coverage report --show-missing --fail-under=100
+coverage report --data-file=/tmp/.coveragerc --show-missing --fail-under=100
