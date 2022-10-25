d1966ba15a0f3b4aebb8ec495fd83de761f27320
Peter Adrichem <Peter.Adrichem@gmail.com>
2022-10-25 16:29:02 +0200

    SQL and mapping fixes.

--- gobconfig/import_/data/sql/brk2/kadastraleobjecten.sql
+++ gobconfig/import_/data/sql/brk2/kadastraleobjecten.sql
@@ -1,1 +1,47 @@
-SELECT * FROM brk2_prepared.kadastraal_object
+WITH kot_full AS (
+    SELECT
+	identificatie,
+	volgnummer,
+	id,
+	registratiedatum,
+	kadastrale_aanduiding,
+	perceelnummer,
+	indexletter,
+	indexnummer,
+	soort_grootte_code,
+	soort_grootte_omschrijving,
+	grootte,
+	soort_cultuur_onbebouwd_code,
+	soort_cultuur_onbebouwd_omschrijving,
+	status,
+	referentie,
+	oudst_digitaal_bekend,
+	mutatie_id,
+	meettarief_verschuldigd,
+	toelichting_bewaarder,
+	tijdstip_ontstaan_object,
+	hoofdsplitsing_identificatie,
+	afwijking_lijst_rechthebbenden,
+	ST_AsText(geometrie) AS geometrie,
+	ST_AsText(plaatscoordinaten) AS plaatscoordinaten,
+	perceelnummer_rotatie,
+	perceelnummer_verschuiving_x,
+	perceelnummer_verschuiving_y,
+	ST_AsText(bijpijling_geometrie) AS bijpijling_geometrie,
+	koopsom,
+	koopsom_valutacode,
+	koopjaar,
+	indicatie_meer_objecten,
+	toestandsdatum,
+	begin_geldigheid,
+	eind_geldigheid,
+	in_onderzoek,
+	datum_actueel_tot,
+	_expiration_date,
+	is_ontstaan_uit_g_perceel,
+	heeft_een_relatie_met_verblijfsobject,
+	is_ontstaan_uit_kadastraalobject
+	FROM brk2_prepared.kadastraal_object kot_full
+)
+SELECT *
+FROM kot_full
