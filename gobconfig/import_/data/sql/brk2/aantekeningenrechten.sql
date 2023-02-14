8e4e358bf388256f0b7d0ab395bbeb236827c769
Peter Adrichem <Peter.Adrichem@gmail.com>
2023-02-14 11:58:57 +0100

    BRK2 aantekeningenrechten 1.0.0.

---
+++ gobconfig/import_/data/sql/brk2/aantekeningenrechten.sql
@@ -0,0 +1,16 @@
+SELECT
+	identificatie,
+	neuron_id,
+	einddatum_recht,
+	aard_code,
+	aard_omschrijving,
+	omschrijving,
+	betreft_gedeelte_van_perceel,
+	betrokken_brk_tenaamstelling,
+	heeft_brk_betrokken_persoon,
+	is_gebaseerd_op_brk_stukdeel,
+	einddatum,
+	datum_actueel_tot,
+	DATE(toestandsdatum) AS toestandsdatum,
+	_expiration_date
+FROM brk2_prepared.aantekening_recht
