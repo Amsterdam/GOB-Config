8470603e0bb5de090e024a2a1a87aaa79942b6d3
Peter Adrichem <Peter.Adrichem@gmail.com>
2023-02-14 12:33:51 +0100

    BRK2 aantekeningenkadastraleobjecten 1.0.0.

---
+++ gobconfig/import_/data/sql/brk2/aantekeningenkadastraleobjecten.sql
@@ -0,0 +1,19 @@
+SELECT
+	identificatie,
+	volgnummer,
+	registratiedatum,
+	begin_geldigheid,
+	eind_geldigheid,
+	einddatum_recht,
+	aard_code,
+	aard_omschrijving,
+	omschrijving,
+	betreft_gedeelte_van_perceel,
+	heeft_brk_betrokken_persoon,
+	heeft_betrekking_op_brk_kadastraal_object,
+	is_gebaseerd_op_brk_stukdeel,
+	einddatum,
+	datum_actueel_tot,
+	DATE(toestandsdatum) AS toestandsdatum,
+	_expiration_date
+FROM brk2_prepared.aantekening_kadastraal_object
