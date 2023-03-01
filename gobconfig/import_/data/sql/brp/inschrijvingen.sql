73e794e078042d01dde9b22274bff68498a573ca
Roel de Vries <roel.de.vries@amsterdam.nl>
2023-03-01 13:19:29 +0100

    Remove `brp` import configuration (cognos)

--- gobconfig/import_/data/sql/brp/inschrijvingen.sql
+++
@@ -1,5 +0,0 @@
-select identificatie
-,      burgerservicenummer
-,      datum_eerste_inschrijving_gba_rni
-,      indicatie_geheim
-from brp_prepared.inschrijvingen
--- gobconfig/import_/data/sql/brp/overlijden.sql
+++
@@ -1,5 +0,0 @@
-select identificatie
-,      datum_overlijden
-,      plaats_overlijden
-,      land_overlijden
-from brp_prepared.overlijden
