880d3a78764a2e27860675338d82230a34f9ae95
Brahim cherghou <brahim@Brahims-MacBook-Pro.local>
2023-06-12 16:46:00 +0200

    merge conflict fixed

--- gobconfig/__init__.py
+++ gobconfig/__init__.py
@@ -2,9 +2,5 @@
 
 
 # GOB Config version.
-<<<<<<< HEAD
 __version_info__ = ("0", "10", "0")
-=======
-__version_info__ = ("0", "9", "6")
->>>>>>> 931e2903c72f5616e2d9a375191c343fdb873219
 __version__ = ".".join(__version_info__)
