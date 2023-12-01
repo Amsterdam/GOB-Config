f55e813059bc7b5728bc8f6fb74c28a5f7c4965d
Roel de Vries <roel.de.vries@amsterdam.nl>
2023-12-01 13:20:59 +0100

    Update monumenten import config

--- gobconfig/import_/data/sql/monumenten/situeringen.sql
+++ gobconfig/import_/data/sql/monumenten/situeringen.sql
@@ -1,6 +1,7 @@
 SELECT identificatie,
-       "hoortBijMonumentenMonument" as hoort_bij_monumenten_monument,
-       "betreftBagNummeraanduiding" as betreft_bag_nummeraanduiding,
-       "eersteSituering"            as eerste_situering,
-       "situeringNummeraanduiding"  as situering_nummeraanduiding
+       "hoortBijMonumentenMonument" AS hoort_bij_monumenten_monument,
+       "betreftBagNummeraanduiding" AS betreft_bag_nummeraanduiding,
+       "eersteSituering"            AS eerste_situering,
+       "situeringNummeraanduiding"  AS situering_nummeraanduiding,
+       "datumActueelTot"            AS datum_actueel_tot
 FROM monumenten_prepared.situeringen
