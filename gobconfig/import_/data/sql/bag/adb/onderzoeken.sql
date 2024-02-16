8bcf5beeb66ccec3bf2eb1e5afaa127e9f035d0d
Jaap Koetsier <jaap@chariot-it.nl>
2024-02-16 09:29:35 +0100

    Add import for BAG OZK from GOBPrepare

---
+++ gobconfig/import_/data/sql/bag/adb/onderzoeken.sql
@@ -0,0 +1,16 @@
+select
+    identificatie,
+    volgnummer,
+    registratiedatum,
+    object_identificatie,
+    objecttype,
+    kenmerk,
+    in_onderzoek,
+    begin_geldigheid,
+    eind_geldigheid,
+    documentnummer,
+    documentdatum,
+    tijdstip_registratie,
+    eind_registratie,
+    _expiration_date
+from bag_adb.bag_onderzoeken;
